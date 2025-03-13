aa9730cea539a5882d752c7dfdd6966bd9b6fdad
Alex Mihov <alexmihov.as@gmail.com>
2025-03-13 13:53:05 +0100

    feat: implement storeReview function in reviews controller for adding new reviews

--- contollers/reviewsControllers.js
+++ contollers/reviewsControllers.js
@@ -2,8 +2,26 @@
 const connection = require("../data/movies_db");
 
 // funzione store 
-
 function storeReview(req, res) {
+    // Estrai l'id dai parametri della richiesta
+    const { id } = req.params;
 
+    // Estrai name, text e vote dal corpo della richiesta
+    const { name, text, vote } = req.body;
 
+    // Query SQL per inserire una nuova recensione
+    const insertReviewSql = 'INSERT INTO reviews (text, name, vote, movie_id) VALUES (?, ?, ?, ?)'
+
+    // Eseguiamo la query 
+    connection.query(insertReviewSql, [text, name, vote, id], (err, reviewsResults) => {
+        // Gestione degli errori della query
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        // Imposta lo stato della risposta a 201 (Creato)
+        res.status(201);
+        // Risponde con un messaggio di successo e l'id della recensione inserita
+        res.json({ message: "Review added", id: reviewsResults.insertId });
+    });
 }
+
+// Esporta la funzione storeReview
+module.exports = { storeReview }
